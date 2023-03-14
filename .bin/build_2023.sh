a826f2740853db1778e0c8c046a368aa08792d64
Carlos Tapasco <carlos.tapasco@wizeline.com>
2023-03-14 14:10:48 -0500

    Adding conditional

--- .bin/build_2023.sh
+++ .bin/build_2023.sh
@@ -14,13 +14,30 @@ function main() {
         exit
     fi
 
-	environment=$1
+	branch=$1
 	commit_id=$2
 
 	commit_id_short=${commit_id::7}
 
 	echo "Branch: $environment"
 	echo "Commit: $commit_id_short"
+
+	if [ "$branch" == "develop" ]
+		then
+			environment=DEVELOP
+			DOCKER_IMAGE_URI=172259620592.dkr.ecr.us-east-1.amazonaws.com/www:${branch}.${commit_id_short}
+	elif [ "$branch" == "staging" ]
+		then
+			environment=STAGING
+			DOCKER_IMAGE_URI=172259620592.dkr.ecr.us-east-1.amazonaws.com/www:${branch}.${commit_id_short}
+	elif [ "$branch" == "master" ]
+		then
+			environment=PRODUCTION
+			DOCKER_IMAGE_URI=172259620592.dkr.ecr.us-east-1.amazonaws.com/www:${branch}.${commit_id_short}
+	else
+		environment="feature"
+		DOCKER_IMAGE_URI=172259620592.dkr.ecr.us-east-1.amazonaws.com/www:feature.${commit_id_short}
+	fi
 
 }
 
